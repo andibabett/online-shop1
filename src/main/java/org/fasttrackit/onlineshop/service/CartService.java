e0339d713ab2c1e79159202ae90116af45ab0167
andibabett <andi_babett@yahoo.com>
2019-12-16 20:32:01 +0200

    added get product method                          added custom queries in spring-boot repositories                          added CRUD endpoints and products                          interactive documentation

--- src/main/java/org/fasttrackit/onlineshop/service/CartService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/CartService.java
@@ -3,13 +3,20 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Cart;
 import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.CartRepository;
 import org.fasttrackit.onlineshop.transfer.AddProductToCartRequest;
+import org.fasttrackit.onlineshop.transfer.CartResponse;
+import org.fasttrackit.onlineshop.transfer.ProductInCartResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 @Service
 public class CartService {
@@ -29,18 +36,16 @@ public class CartService {
 
     @Transactional
     public void addProductToCart(AddProductToCartRequest request) {
-        LOGGER.info("Adding product to cart: {}")
-        Cart cart = cartRepository.findById(request.getCustomerId())
-                .orElse(new Cart());
+        LOGGER.info("Adding product to cart: {}", request);
+
+        Cart cart = cartRepository.findById(request.getCustomerId()).orElse(new Cart());
 
         if (cart.getCustomer() == null) {
-            LOGGER.info("New cart will be created. " +
-                    "Retrieving customer {} to map the relationship",
-                    request.getCustomerId());
+            LOGGER.info("New cart will be created. " + "Retrieving customer {} to map the relationship", request.getCustomerId());
 
-            Customer customer =
-                    customerService.getCustomer(request.getCustomerId());
+            Customer customer = customerService.getCustomer(request.getCustomerId());
 
+            cart.setId(customer.getId());
             cart.setCustomer(customer);
         }
         Product product = productService.getProduct(request.getProductId());
@@ -49,5 +54,32 @@ public class CartService {
         cartRepository.save(cart);
     }
 
-    public Cart getCart();
+    @Transactional
+    public CartResponse getCart(long id) {
+        LOGGER.info("Retrievering cart {}", id);
+
+        Cart cart = cartRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cart " + id + "does not exist"));
+
+        CartResponse response = new CartResponse();
+        response.setId(cart.getId());
+
+        Set<ProductInCartResponse> productsInCart = new HashSet<>();
+
+        Iterator<Product> cartIterator = cart.getProducts().iterator();
+
+        while (cartIterator.hasNext()) {
+            Product product = cartIterator.next();
+
+            ProductInCartResponse productResponse = new ProductInCartResponse();
+            productResponse.setId(product.getId());
+            productResponse.setName(product.getName());
+            productResponse.setPrice(product.getPrice());
+
+            productsInCart.add(productResponse);
+        }
+
+        response.setProducts(productsInCart);
+        return response;
+
+    }
 }
--- src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
@@ -1,14 +1,24 @@
 package org.fasttrackit.onlineshop;
 
 import org.fasttrackit.onlineshop.domain.Customer;
+import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.CartService;
 import org.fasttrackit.onlineshop.steps.CustomerSteps;
+import org.fasttrackit.onlineshop.steps.ProductSteps;
 import org.fasttrackit.onlineshop.transfer.AddProductToCartRequest;
+import org.fasttrackit.onlineshop.transfer.CartResponse;
+import org.fasttrackit.onlineshop.transfer.ProductInCartResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,13 +30,33 @@ public class CartServiceIntegrationTests {
     @Autowired
     private CustomerSteps customerSteps;
 
+    @Autowired
+    private ProductSteps productSteps;
+
     @Test
     public void testAddProductToCart_whenNewCartForExistingCustomer_thenCartIsSaved() {
         Customer customer = customerSteps.createCustomer();
+        Product product = productSteps.createProduct();
 
         AddProductToCartRequest request = new AddProductToCartRequest();
         request.setCustomerId(customer.getId());
+        request.setProductId(product.getId());
 
         cartService.addProductToCart(request);
+
+        CartResponse cart = cartService.getCart(customer.getId());
+
+        assertThat(cart.getId(), is(customer.getId()));
+
+        Iterator<ProductInCartResponse> iterator = cart.getProducts().iterator();
+
+        assertThat(iterator.hasNext(), is(true));
+
+        ProductInCartResponse productFromCart = iterator.next();
+
+        assertThat(productFromCart, notNullValue());
+        assertThat(productFromCart.getId(), is(product.getId()));
+        assertThat(productFromCart.getName(), is(product.getName()));
+        assertThat(productFromCart.getPrice(), is(product.getPrice()));
     }
 }
